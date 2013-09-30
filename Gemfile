bb9ade37db0ff0d2131b758d555cb524376984cd
Garrett LeSage <garrett@lesage.us>
2013-09-30 11:40:44 +0200

    added our configuration settings and gems

--- Gemfile
+++ Gemfile
@@ -7,10 +7,58 @@ gem "middleman", "~>3.1.5"
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
 
-# For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
-
 # Cross-templating language block fix for Ruby 1.8
 platforms :mri_18 do
   gem "ruby18_source_location"
 end
+
+# For faster file watcher updates for people using Windows
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+
+
+#####
+# General plugins
+
+# Blog plugin
+gem "middleman-blog", "~> 3.3.0"
+
+# favicon support (favicon PNG should be 144×144)
+gem "middleman-favicon-maker"
+
+# HTML & XML parsing smarts
+gem "nokogiri"
+
+# Syntax highlighting
+gem "middleman-syntax"
+
+# For feed.xml.builder
+gem "builder", "~> 3.0"
+
+
+#####
+# Bootstrap
+
+# Bootstrap, as SASS 3 version (from master branch, until official release):
+gem "bootstrap-sass", github: "thomas-mcdonald/bootstrap-sass"
+
+# Bootstrap 3 version (from a fork; temporary also):
+gem "bootstrap-navbar", github: "delynn/bootstrap-navbar"
+# Bootstrap-navbar integration for Middleman
+gem "middleman-bootstrap-navbar"
+
+
+#####
+# Formats
+
+# less (css)
+gem "therubyracer"
+gem "less"
+
+# asciidoctor
+gem "asciidoctor"
+
+# mediawiki
+gem "wikicloth"
+
+# Markdown, with GitHub flavoring
+gem "redcarpet"
--- config.rb
+++ config.rb
@@ -1,4 +1,84 @@
 ###
+# Site settings
+###
+
+set :site_name, "Software Collections"
+set :site_logo, "softwarecollections-logo.png"
+set :site_domain, "softwarecollections.org"
+
+Time.zone = "UTC"
+
+# Make pretty URLs
+activate :directory_indexes
+
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+# Syntax highlighting
+activate :syntax
+
+# Make URLs relative
+set :relative_links, true
+
+# Set HAML to render HTML5 by default (when unspecified)
+set :haml, { :format => :html5 }
+
+# Set Markdown features for RedCarpet
+# (So our version of Markdown resembles GitHub's)
+set :markdown, {
+  :tables => true,
+  :autolink => true,
+  :gh_blockcode => true,
+  :fenced_code_blocks => true,
+  :smartypants => true
+}
+set :markdown_engine, :redcarpet
+
+# Set directories
+set :css_dir, 'stylesheets'
+set :fonts_dir, 'stylesheets/fonts'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+set :partials_dir, 'layouts'
+
+
+
+###
+# Blog settings
+###
+
+activate :blog do |blog|
+  blog.prefix = "blog/"
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  blog.default_extension = ".md"
+  blog.sources = ":year-:month-:day-:title.html"
+  blog.permalink = ":year/:month/:day/:title.html"
+  blog.year_link = ":year.html"
+  blog.month_link = ":year/:month.html"
+  blog.day_link = ":year/:month/:day.html"
+
+  blog.layout = "blog_layout"
+
+  #blog.taglink = "tags/:tag.html"
+
+  #blog.summary_separator = /(READMORE)/
+  #blog.summary_length = 250
+
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/:num"
+end
+
+# Enable blog layout for all blog pages
+with_layout :blog_layout do
+  page "/blog.html"
+  page "/blog/*"
+end
+
+
+###
 # Compass
 ###
 
@@ -6,6 +86,7 @@
 # compass_config do |config|
 #   config.output_style = :compact
 # end
+
 
 ###
 # Page options, layouts, aliases and proxies
@@ -24,9 +105,14 @@
 #   page "/admin/*"
 # end
 
+# Don't have a layout for XML
+page "/feed.xml", :layout => false
+page "/sitemap.xml", :layout => false
+
 # Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+
 
 ###
 # Helpers
@@ -45,26 +131,56 @@
 #   end
 # end
 
-set :css_dir, 'css'
 
-set :js_dir, 'js'
+###
+# Development-only configuration
+###
+#
+configure :development do
+  activate :livereload
+  #config.sass_options = {:debug_info => true}
+  #config.sass_options = {:line_comments => true}
+  compass_config do |config|
+    config.output_style = :expanded
+    config.sass_options = {:debug_info => true, :line_comments => true}
+  end
+end
 
-set :images_dir, 'img'
+
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  ## Ignore Gimp source files
+  ignore 'images/*.xcf*'
 
-  # Minify Javascript on build
-  # activate :minify_javascript
+  # Don't export source JS
+  ignore 'javascripts/vendor/*'
+  ignore 'javascripts/lib/*'
 
-  # Enable cache buster
+  # Don't export source CSS
+  ignore 'stylesheets/vendor/*'
+  ignore 'stylesheets/lib/*'
+
+  # Minify JavaScript and CSS on build
+  activate :minify_javascript
+  activate :minify_css
+
+  # Force a browser reload for new content by using
+  # asset_hash or cache buster (but not both)
+  activate :cache_buster
   # activate :asset_hash
 
-  # Use relative URLs
-  # activate :relative_assets
+  # Use relative URLs for all assets
+  activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
 
   # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  # set :http_path, "/Content/images/"
+
+  # Favicon PNG should be 144×144 and in source/favicon_base.png
+  activate :favicon_maker
 end

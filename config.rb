3e3742cfa6f8db1a8a60a3b08dfc618dfcf4fb5d
Garrett LeSage <garrett@lesage.us>
2013-10-07 22:51:43 +0200

    major changes for "nav", for docs

--- config.rb
+++ config.rb
@@ -119,6 +119,12 @@ end
 page "/feed.xml", :layout => false
 page "/sitemap.xml", :layout => false
 
+# Docs all have the docs layout
+with_layout :docs do
+  page "/documentation/*"
+  page "/documentation*"
+end
+
 # Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
@@ -141,7 +147,7 @@ page "/sitemap.xml", :layout => false
 #   end
 # end
 helpers do
-  def find_url(dirty_URL)
+  def normalize_url(dirty_URL)
     r = url_for Middleman::Util.normalize_path(dirty_URL)
     r.sub(/\/$/, '')
   end

3543ce265c95a5dd6f353a555f09be9889c36e44
Garrett LeSage <garrett@lesage.us>
2013-11-19 13:41:22 +0100

    changed .htaccess to be generated by HAML & added in blog URL redirects

--- config.rb
+++ config.rb
@@ -130,6 +130,8 @@ end
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+proxy "/.htaccess", "/.htaccess.html", :locals => {}, :ignore => true
+
 
 ###
 # Helpers

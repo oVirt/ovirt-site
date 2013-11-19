4091830d3d7999f7269f4a0f5aabc6a4db6eb753
Garrett LeSage <garrett@lesage.us>
2013-11-19 17:17:21 +0100

    don't make the 404 page have a pretty URL

--- config.rb
+++ config.rb
@@ -126,6 +126,10 @@ with_layout :docs do
   page "/documentation*"
 end
 
+# Don't make these URLs have pretty URLs
+page "/404.html", :directory_index => false
+page "/.htacces.html", :directory_index => false
+
 # Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }

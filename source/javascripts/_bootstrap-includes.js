5ae49711c679197ee69fff6e10483f65740b9eec
Garrett LeSage <garrett@lesage.us>
2014-09-17 12:14:41 +0200

    adjust JS & CSS for admin section

--- source/javascripts/_bootstrap-includes.js
+++ source/javascripts/_bootstrap-includes.js
@@ -1,14 +1,14 @@
 // Change /// to //= to enable an include
 
-/// require bootstrap/affix
-/// require bootstrap/alert
-/// require bootstrap/button
-/// require bootstrap/carousel
-/// require bootstrap/collapse
-/// require bootstrap/dropdown
-/// require bootstrap/tab
-/// require bootstrap/transition
-/// require bootstrap/scrollspy
-/// require bootstrap/modal
-/// require bootstrap/tooltip
-/// require bootstrap/popover
+//= require bootstrap/affix
+//= require bootstrap/alert
+//= require bootstrap/button
+//= require bootstrap/carousel
+//= require bootstrap/collapse
+//= require bootstrap/dropdown
+//= require bootstrap/tab
+//= require bootstrap/transition
+//= require bootstrap/scrollspy
+//= require bootstrap/modal
+//= require bootstrap/tooltip
+//= require bootstrap/popover

0ed8b8f983e3834b33d21e94374edf1d2f3e50f9
Garrett LeSage <garrett@lesage.us>
2014-09-30 14:09:49 +0200

    support tags

--- create-post.rb
+++ create-post.rb
@@ -14,6 +14,7 @@ opts = Slop.parse!(help: true, ignore_case: true) do
   on 'a', 'author', "Author name", argument: :required, default: ENV['USER']
   on 'd', 'date', "Custom datetime", argument: :required, default: Time.now.strftime('%F %T %Z')
   on 'e', 'editor', "Custom editor", argument: :required, default: ENV['EDITOR']
+  on 't', 'tags', "Comma-separated list of tags", argument: :required
   on 'n', 'no-browser', "Disable browser"
 end
 
@@ -21,11 +22,14 @@ end
 # so we're left with the title (either as a string or in parts)
 title = ARGV.join(' ')
 
+# Normalize CSV
+tags = opts[:tags] ? opts[:tags].gsub(/,\s*/, ", ") : nil
 
 # Frontmatter
 meta = {
   'title' => title,
   'author' => opts[:author],
+  'tags' => tags,
   'date' => (Chronic.parse(opts[:date]) || Time.now).strftime('%F %T %Z')
 }
 

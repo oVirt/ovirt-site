637b81fbfeb6f42c135c5794f2bb2d0a475e0926
Garrett LeSage <garrett@lesage.us>
2014-10-09 17:20:20 +0200

    added $UID mapping into docker

--- run-docker.sh
+++ run-docker.sh
@@ -4,4 +4,4 @@ DOCKERID=`cat .dockerid`
 
 echo "Running docker image $DOCKERID"
 
-docker run -v $PWD:/opt/website -p 4567:4567 -it $DOCKERID $@
+docker run -u $UID -v $PWD:/opt/website -p 4567:4567 -it $DOCKERID $@

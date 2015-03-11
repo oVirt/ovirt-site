a0ce00ea505ff01008ce88e93d303b69f03358a7
Garrett LeSage <garrett@lesage.us>
2015-03-11 14:12:14 +0100

    changed configs based on rubocop suggestions

--- config.rb
+++ config.rb
@@ -4,8 +4,7 @@
 
 # Look in data/site.yml for general site configuration
 
-
-Time.zone = data.site.timezone || "UTC"
+Time.zone = data.site.timezone || 'UTC'
 
 # Automatic image dimensions on image_tag helper
 activate :automatic_image_sizes
@@ -18,25 +17,25 @@ set :relative_links, true
 
 # Set HAML to render HTML5 by default (when unspecified)
 # It's important HAML outputs "ugly" HTML to not mess with code blocks
-set :haml, :format => :html5, :ugly => true
+set :haml, format: :html5, ugly: true
 
 # Set Markdown features for Kramdown
 # (So our version of Markdown resembles GitHub's w/ other nice stuff)
 set :markdown,
-  transliterated_header_ids: true,
-  parse_block_html: true,
-  parse_span_html: true,
-  tables: true,
-  hard_wrap: false,
-  input: 'GFM' # add in some GitHub-flavor (``` for fenced code blocks)
+    transliterated_header_ids: true,
+    parse_block_html: true,
+    parse_span_html: true,
+    tables: true,
+    hard_wrap: false,
+    input: 'GFM' # add in some GitHub-flavor (``` for fenced code blocks)
 
 set :markdown_engine, :kramdown
 
 set :asciidoc_attributes, %w(source-highlighter=coderay imagesdir=images)
 
 set :asciidoctor,
-  :toc => true,
-  :numbered => true
+    toc: true,
+    numbered: true
 
 # Set directories
 set :css_dir, 'stylesheets'
@@ -45,48 +44,45 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :partials_dir, 'layouts'
 
-
 ###
 # Blog settings
 ###
 
 activate :blog do |blog|
-  blog.prefix = "blog/"
-  blog.layout = "post"
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
-  blog.default_extension = ".md"
+  blog.prefix = 'blog/'
+  blog.layout = 'post'
+  blog.tag_template = 'tag.html'
+  blog.calendar_template = 'calendar.html'
+  blog.default_extension = '.md'
 
-  blog.sources = ":year-:month-:day-:title.html"
-  #blog.permalink = ":year/:month/:day/:title.html"
-  blog.permalink = ":year/:month/:title.html"
-  blog.year_link = ":year.html"
-  blog.month_link = ":year/:month.html"
-  #blog.day_link = ":year/:month/:day.html"
+  blog.sources = ':year-:month-:day-:title.html'
+  # blog.permalink = ":year/:month/:day/:title.html"
+  blog.permalink = ':year/:month/:title.html'
+  blog.year_link = ':year.html'
+  blog.month_link = ':year/:month.html'
+  # blog.day_link = ":year/:month/:day.html"
 
+  blog.taglink = 'tag/:tag.html'
 
-  blog.taglink = "tag/:tag.html"
-
-  #blog.summary_separator = /(READMORE)/
-  #blog.summary_length = 99999
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 99999
 
   blog.paginate = true
   blog.per_page = 10
-  blog.page_link = "page=:num"
+  blog.page_link = 'page=:num'
 end
 
-#activate :authors
-#activate :drafts
+# activate :authors
+# activate :drafts
 
 # Enable blog layout for all blog pages
 with_layout :post do
-  page "/blog.html"
-  page "/blog/*"
+  page '/blog.html'
+  page '/blog/*'
 end
 
 # Make pretty URLs
 activate :directory_indexes
-
 
 ###
 # Compass
@@ -96,7 +92,6 @@ activate :directory_indexes
 # compass_config do |config|
 #   config.output_style = :compact
 # end
-
 
 ###
 # Page options, layouts, aliases and proxies
@@ -116,39 +111,50 @@ activate :directory_indexes
 # end
 
 # Don't have a layout for XML
-page "*.xml", :layout => false
+page '*.xml', layout: false
 
 # Docs all have the docs layout
 with_layout :docs do
-  #page "/documentation/*"
-  #page "/documentation*"
+  # page "/documentation/*"
+  # page "/documentation*"
 end
 
 # Don't make these URLs have pretty URLs
-page "/404.html", :directory_index => false
-page "/.htacces.html", :directory_index => false
+page '/404.html', directory_index: false
+page '/.htacces.html', directory_index: false
 
 # Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-proxy "/.htaccess", "/.htaccess.html", :locals => {}, :ignore => true
+proxy '/.htaccess', '/.htaccess.html', locals: {}, ignore: true
 
 ready do
   # Add author pages
-  sitemap.resources.group_by {|p| p.data["author"]}.each do |author, pages|
-    proxy "/blog/author/#{author.parameterize.downcase}.html", "author.html", locals: {author: author, pages: pages}, :ignore => true if author
+  sitemap.resources.group_by { |p| p.data['author'] }.each do |author, pages|
+    next unless author
+
+    proxy "/blog/author/#{author.parameterize.downcase}.html",
+          'author.html',
+          locals: { author: author, pages: pages },
+          ignore: true
   end
-  proxy "/blog/author.html", "author.html", :ignore => true
+
+  proxy '/blog/author.html', 'author.html', ignore: true
 
   # Add blog feeds
-  blog.tags.each do |tag_name, tag_data|
-    proxy "/blog/tag/#{tag_name.downcase}.xml", "feed.xml", locals: {tag_name: tag_name}, :ignore => true if tag_name
-  end
-  proxy "/blog/feed.xml", "feed.xml", :ignore => true
-  proxy "/blog/tag/index.html", "tag.html", :ignore => true
-end
+  blog.tags.each do |tag_name, _tag_data|
+    next unless tag_name
 
+    proxy "/blog/tag/#{tag_name.downcase}.xml",
+          'feed.xml',
+          locals: { tag_name: tag_name },
+          ignore: true
+  end
+
+  proxy '/blog/feed.xml', 'feed.xml', ignore: true
+  proxy '/blog/tag/index.html', 'tag.html', ignore: true
+end
 
 ###
 # Helpers
@@ -160,8 +166,8 @@ end
 #     "Helping"
 #   end
 # end
-#helpers do
-#end
+# helpers do
+# end
 
 require 'lib/site_helpers.rb'
 activate :site_helpers
@@ -170,7 +176,6 @@ require 'lib/blog_helpers.rb'
 activate :blog_helpers
 
 require 'lib/monkeypatch_blog_date.rb'
-
 
 ###
 # Development-only configuration
@@ -181,14 +186,14 @@ configure :development do
   puts `git submodule init && git submodule sync`
   puts `git submodule foreach "git pull -qf origin master"`
   puts "\n"
-  puts "== Administration is at http://0.0.0.0:4567/admin/"
+  puts '== Administration is at http://0.0.0.0:4567/admin/'
 
   activate :livereload
-  #config.sass_options = {:debug_info => true}
-  #config.sass_options = {:line_comments => true}
+  # config.sass_options = {:debug_info => true}
+  # config.sass_options = {:line_comments => true}
   compass_config do |config|
     config.output_style = :expanded
-    config.sass_options = {:debug_info => true, :line_comments => true}
+    config.sass_options = { debug_info: true, line_comments: true }
   end
 end
 
@@ -200,9 +205,9 @@ configure :build do
   puts "\n"
 
   ## Ignore administration UI
-  ignore "/admin/*"
-  ignore "/javascripts/admin*"
-  ignore "/stylesheets/lib/admin*"
+  ignore '/admin/*'
+  ignore '/javascripts/admin*'
+  ignore '/stylesheets/lib/admin*'
 
   ## Ignore Gimp source files
   ignore 'images/*.xcf*'
@@ -220,7 +225,7 @@ configure :build do
   # Minify JavaScript and CSS on build
   activate :minify_javascript
   activate :minify_css
-  #activate :gzip
+  # activate :gzip
 
   # Force a browser reload for new content by using
   # asset_hash or cache buster (but not both)
@@ -228,7 +233,7 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs for all assets
-  #activate :relative_assets
+  # activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
@@ -241,17 +246,16 @@ configure :build do
   # Favicon PNG should be 144×144 and in source/images/favicon_base.png
   # Note: You need ImageMagick installed for favicon_maker to work
   activate :favicon_maker do |f|
-    f.template_dir  = File.join(root, 'source','images')
-    f.output_dir    = File.join(root, 'build','images')
+    f.template_dir  = File.join(root, 'source', 'images')
+    f.output_dir    = File.join(root, 'build', 'images')
     f.icons = {
-        "favicon_base.png" => [
-                { icon: "favicon.png", size: "16x16" },
-                { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
-        ]
+      'favicon_base.png' => [
+        { icon: 'favicon.png', size: '16x16' },
+        { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' }
+      ]
     }
   end
 end
-
 
 ###
 # Deployment
@@ -297,12 +301,12 @@ else
 
   deploy_config = {
     method: :git,
-    remote: "production",
-    branch: "master",
+    remote: 'production',
+    branch: 'master',
     build_before: true # default false
   }
 end
 
 activate :deploy do |deploy|
-  deploy_config.each {|key, val| deploy[key] = val }
+  deploy_config.each { |key, val| deploy[key] = val }
 end

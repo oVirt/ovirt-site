e8c84fd81c27c567f2330a4b678331b0bab66f3a
Garrett LeSage <garrett@lesage.us>
2015-07-03 15:33:48 +0200

    added preliminary category page support

--- config.rb
+++ config.rb
@@ -159,6 +159,11 @@ ready do
 
   proxy '/blog/feed.xml', 'feed.xml', ignore: true
   proxy '/blog/tag/index.html', 'tag.html', ignore: true
+
+  sitemap.resources.reject { |p| p.data.wiki_title.nil? }.each do |p|
+    next unless p.data.wiki_title.match(/^category:/i)
+    page p.path, layout: 'category'
+  end
 end
 
 ###

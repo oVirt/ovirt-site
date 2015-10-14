592bbc288dc4e0a7c3b8dc28e867efc33c84bb4f
Garrett LeSage <garrett@lesage.us>
2015-10-14 12:47:56 +0200

    many changes to support events more properly (style, tooltip, etc.)

--- source/javascripts/lib/cal-widget.js
+++ source/javascripts/lib/cal-widget.js
@@ -2,6 +2,29 @@ $(function() {
   $widget = $('#calendar-widget')
 
   if ($widget.length) {
+    tip = '<div id="fc-tooltip" class="fc-tooltip"></div>'
+    $widget.after(tip);
+    $tip = $('#fc-tooltip');
+
+    var mouseOver = function(event, jsEvent, view) {
+      var $item = $(this);
+
+      var eventText = '<b>' + event.title + '</b><br>' + 'Time: ' + event.start.format('h:mm a z') + '<br>Location: ' + event.location;
+
+      $tip.html(eventText).show();
+
+      var left = $item.offset()['left'] - $item.width() - $tip.width();
+      var top = $item.offset()['top'];
+
+      $tip.offset({top: top, left: left});
+      console.log(event.title);
+      console.log([event, jsEvent, view]);
+      console.log($item.offset());
+    };
+
+    var mouseOut = function(event, jsEvent, view) {
+      $tip.hide().html('');
+    };
 
     $.get('/events/calendar.json', function(data){
       $widget.fullCalendar({
@@ -12,11 +35,13 @@ $(function() {
           left: 'title',
           firstDay: 1,
           center: '',
-          //aspectRatio: 1,
-          right: 'today prev,next'
+          aspectRatio: 1,
+          right: 'today prev,next'// month,agendaWeek'
         },
         //eventColor: '#44aaff',
-        events: data
+        events: data,
+        eventMouseover: mouseOver,
+        eventMouseout: mouseOut
       });
     });
 

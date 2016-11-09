188a89ea45480449659fa493a4be8ec6ef3ba1a9
bproffit <bproffit@fedora.federation>
2016-11-09 11:36:22 -0500

    Added bootstrap directory with RHV4 documents

---
+++ source/documentation/bootstrap/doc-Introduction_to_the_Administration_Portal/en-US/files/pressgang_website.js
@@ -0,0 +1,9 @@
+pressgang_website_callback([
+	{"topicId":44912,"target":"appe-Revision_History","title":"Revision History","newSince":""},
+	{"topicId":34614,"target":"Author_Group","title":"Author Group","newSince":""},
+	{"topicId":7848,"target":"Red_Hat_Enterprise_Virtualization_Manager_Client_Requirements","title":"Browser and Client Requirements","newSince":""},
+	{"topicId":8748,"target":"Graphical_User_Interface_elements","title":"Graphical User Interface Elements","newSince":""},
+	{"topicId":8749,"target":"Tree_Mode_and_Flat_Mode","title":"Tree Mode and Flat Mode","newSince":""},
+	{"topicId":8750,"target":"Using_the_Guide_Me_facility","title":"Using the Guide Me Facility","newSince":""},
+	{"topicId":8751,"target":"Performing_searches_in_Red_Hat_Enterprise_Virtualization","title":"Performing Searches in Red Hat Enterprise Virtualization","newSince":""},
+	{"topicId":7295,"target":"To_save_a_query_string_as_a_Bookmark","title":"Saving a Query String as a Bookmark","newSince":""}], 23144, "Red Hat Enterprise Virtualization", "Introduction to the Administration Portal", "3.6-Beta");

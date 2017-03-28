4b3d2982c58530822c77964840254be1df4f4fd3
Laura Hampton <lhampton@gmail.com>
2017-03-28 15:59:33 -0400

    made suggested changes

--- setup.sh
+++ setup.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-#setup for redhat
+#setup for RedHat
 if [ -f /etc/redhat-release ]; then
   git submodule init && git submodule update
 
   sudo yum install -y ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch zlib-devel redhat-rpm-config openssl
 
-  bundle install
-# setup for debian 
+# setup for Debian 
 elif [ -f /etc/debian_version ]; then
   git submodule init && git submodule update
 
   sudo apt install -y build-essential ruby-bundler libcurl4-openssl-dev zlib1g-dev ruby-dev
 
-  bundle install 
-
 else
-    echo "Could not verify system is redhat or debian."
+    echo "Could not verify system is RedHat or Debian."
     exit 1
 fi 
+  bundle install

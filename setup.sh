4d8108e88fd1fdd7f14550e314db9bf712bf4d0a
Laura Hampton <lhampton@gmail.com>
2017-03-29 13:40:54 -0400

    refactored

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
+git submodule init && git submodule update
 #setup for RedHat
 if [ -f /etc/redhat-release ]; then
-  git submodule init && git submodule update
-
+  
   sudo yum install -y ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch zlib-devel redhat-rpm-config openssl
 
 # setup for Debian 
 elif [ -f /etc/debian_version ]; then
-  git submodule init && git submodule update
-
+  
   sudo apt install -y build-essential ruby-bundler libcurl4-openssl-dev zlib1g-dev ruby-dev
 
 else

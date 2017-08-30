2f3b873d563a7fbffddc4f5c4c413572cd440052
eldanhildesheim <info@eldanet.com>
2017-08-30 15:13:44 +0300

    Changing to a better font. Adding Expand/Collapse Feature menu Adding Expand/Collapse for each feature Category All menus are working will local storage to recall last state

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -10,3 +10,100 @@
 //= require _bootstrap-includes
 
 //= require bootstrap-sortable
+var expandIcon =   "▾";
+var collapseIcon = "▸";
+
+var stateToggleLeftAreaOpen;
+
+$( document ).ready(function() {
+  stateToggleLeftAreaOpen = getCookie("stateToggleLeftAreaOpen");
+  handleToggleLeftArea();
+});
+$( "#toggleLeftArea" ).click(function() {
+  handleToggleLeftArea();
+});
+
+function handleToggleLeftArea (){
+  setCookie("stateToggleLeftAreaOpen",stateToggleLeftAreaOpen);
+  if (stateToggleLeftAreaOpen) {
+      $('#toggleLeftArea').html(expandIcon+ " Features List");
+      $('.feature-sidebar').show();
+      $('.feature-info').removeClass( "col-md-12" ).addClass( "col-md-8" );
+  } else {
+
+      $('#toggleLeftArea').html(collapseIcon+" Features List");
+      $('.feature-sidebar').hide();
+      $('.feature-info').removeClass( "col-md-8" ).addClass( "col-md-12" );
+  }
+  stateToggleLeftAreaOpen = !stateToggleLeftAreaOpen;
+}
+
+
+var arrStateToggleOpenCategories = {};
+var enableUI = true;
+function restoreStateToggleOpenCategories(){
+  $( document ).ready(function() {
+    arrStateToggleOpenCategories = getCookie("arrStateToggleOpenCategories");
+    if (arrStateToggleOpenCategories == null) {
+        arrStateToggleOpenCategories = {};
+    }
+    $('.feature_list_in_category').hide();
+    $('.collapseExpandSection').html (collapseIcon);
+    if (enableUI) {
+      for (featureCategory in arrStateToggleOpenCategories) {
+        if (arrStateToggleOpenCategories[featureCategory]) {
+          var el = $('#' + featureCategory);
+          handleToggleFeatureCategory(el, true);
+          }
+      }
+    }
+  });
+}
+restoreStateToggleOpenCategories();
+
+function handleSearchActive(){
+  if (enableUI) {
+    $('.feature_list_in_category').show();
+    $('.collapseExpandSection').html (expandIcon);
+  }
+  enableUI = false;
+}
+
+function handleSearchDeactivate(){
+  enableUI = true;
+  restoreStateToggleOpenCategories();
+}
+
+$( ".ovirtToggleFeatureCategory" ).click(function() {
+  if (enableUI) {handleToggleFeatureCategory($(this),false);}
+});
+
+function handleToggleFeatureCategory(scope, isCookie){
+  scope.parent().find('.feature_list_in_category').toggle( 0, function() {
+    var catName = scope.parent().find(".ovirtToggleFeatureCategory")[0].id;
+    var catValue = isCookie ?  true : scope.parent().find('.feature_list_in_category').is( ":visible" );
+    arrStateToggleOpenCategories ["catName"] = catValue;
+    if (catValue) {
+      (scope.parent().find('.collapseExpandSection')).html (expandIcon);
+      arrStateToggleOpenCategories[catName] = true;
+    } else {
+      (scope.parent().find('.collapseExpandSection')).html (collapseIcon);
+      delete arrStateToggleOpenCategories[catName];
+    }
+    setCookie("arrStateToggleOpenCategories", arrStateToggleOpenCategories);
+  });
+}
+
+
+
+
+
+
+function setCookie(cname, cvalue) {
+    localStorage.setItem(cname, JSON.stringify(cvalue));
+}
+
+
+function getCookie(cname) {
+  return JSON.parse(localStorage.getItem(cname));
+}

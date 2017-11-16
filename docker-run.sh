2fc321e1d8ad68135626191c3e4cc6eb647d4c39
Jakub Niedermertl <jniederm@redhat.com>
2017-11-16 23:58:30 +0100

    SELinux labeling fix for container setup
    If selinux was enabled, volume defined in script docker-run.sh was
    mounted but inaccessible. 'z' volume flag instructs docker to fix the
    labeling.
    
    Reference doc: https://docs.docker.com/engine/admin/volumes/bind-mounts/#configure-the-selinux-label
    Details: https://www.projectatomic.io/blog/2015/06/using-volumes-with-docker-can-cause-problems-with-selinux/

--- docker-run.sh
+++ docker-run.sh
@@ -4,4 +4,4 @@ DOCKERID=`cat .dockerid`
 
 echo "Running docker image $DOCKERID"
 
-docker run -u $UID -v $PWD:/opt/website -p 4567:4567 -it $DOCKERID $@
+docker run -u $UID -v $PWD:/opt/website:z -p 4567:4567 -it $DOCKERID $@

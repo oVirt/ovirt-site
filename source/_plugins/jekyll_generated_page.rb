8054975dbba3d8bbb992e8d8e5b658c40628f17c
Marc Dequènes (Duck) <duck@redhat.com>
2019-08-07 01:48:56 +0900

    jekyll: rework events pages
    also fix the ICS generation "by mistake" (ICS file was generated but
    empty of events).

---
+++ source/_plugins/jekyll_generated_page.rb
@@ -0,0 +1,17 @@
+# using PageWithoutAFile directly is not sufficient to generate a page
+# fully without any interference of templating and other processing
+# altering newline types etc
+class JekyllGeneratedPage < ::Jekyll::PageWithoutAFile
+  def initialize(site, dir, name, content)
+    @site = site
+    @dir = dir
+    @name = name
+    @content = content
+
+    @base = site.source
+    self.process(@name)
+
+    self.content = @content
+    self.data = {}
+  end
+end
---
+++ source/_plugins/ovirt_events_pages.rb
@@ -0,0 +1,27 @@
+require_relative '../../lib/confcal'
+
+module OVirt
+  class EventsPagesGenerator < ::Jekyll::Generator
+    def generate(site)
+      # yearly pages
+      site.data.events.keys.each do |year|
+        next unless year =~ /^\d+$/
+
+        page = ::Jekyll::PageWithoutAFile.new(site, site.source, 'events', "#{year}.html")
+        page.content = "{% haml events_index.haml %}"
+        page.data = {
+          'layout' => 'application',
+          'title' => "#{year} Events",
+          'year' => year,
+        }
+        site.pages << page
+      end
+
+      # JSON data file for the calendar widget
+      site.pages << JekyllGeneratedPage.new(site, 'events', 'calendar.json', gen_calendar_json(site))
+
+      # ICS file
+      site.pages << JekyllGeneratedPage.new(site, 'events', 'all.ics', gen_calendar_ics(site))
+    end
+  end
+end

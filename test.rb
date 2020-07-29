f8953c33618981602794711b1ce59babf63e9afe
Sandro Bonazzola <sbonazzo@redhat.com>
2020-07-29 17:41:47 +0200

    automation: run htmlproofer in parallel (#2294)
    * automation: run htmlproofer in parallel
    
    * fix missing checks_to_ignore

---
+++ test.rb
@@ -0,0 +1,15 @@
+require 'html-proofer'
+
+options = {
+  assume_extension: true,
+  check_img_http: true,
+  checks_to_ignore: [ "LinkCheck" ],
+  empty_alt_ignore: true,
+  allow_hash_href: true,
+  http_status_ignore: [429],
+  file_ignore: [ "/events/" ],
+  url_ignore: [ "https://github.com/oVirt/.*/edit/.*" ],
+  parallel: { in_processes: 8 }
+}
+
+HTMLProofer.check_directory("./_site", options).run

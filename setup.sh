9f9b07eb07a052e551aa749b96356edd7a5e8123
Marc Dequènes (Duck) <duck@redhat.com>
2021-04-07 19:32:39 +0900

    Switch to GH actions and test on prod system
    The goal is to be more integrated. The CI currently is associated with a
    specific user account on travis-ci.org but it's better for the team to
    have more control over it (or in case the person leaves the project).
    
    I also made changes to test the website on the same OS used by the
    production builder. This way changes to the Gemfile can be tested by the
    team directly.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,13 @@
 git submodule init && git submodule update
 #setup for RedHat
 if [ -f /etc/redhat-release ]; then
-  sudo yum install -y epel-release
-  sudo yum install -y --enablerepo=epel ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch zlib-devel redhat-rpm-config openssl nodejs ImageMagick make
+  if [ -f /etc/fedora-release ]; then
+    INSTCMD="dnf install -y"
+  else
+    INSTCMD="yum install -y --enablerepo=epel"
+    sudo $INSTCMD epel-release
+  fi
+  sudo $INSTCMD ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch zlib-devel redhat-rpm-config openssl nodejs ImageMagick make
 
 # setup for Debian
 elif [ -f /etc/debian_version ]; then
